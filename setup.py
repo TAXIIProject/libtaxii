1e5b51a411260724068ff39c33d2476a353de3c4
Mike Goffin <mgoffin@mitre.org>
2013-04-24 10:56:05 -0400

    Updating setup to include more stuff.
    It now includes the necessary xsd files used in the codebase, as well as
    checking for a minimum Python version and appropriate dependencies.
    
    Added an INSTALL file with dependency information and instructions, as well as a
    README which could be included in setup if we wanted.

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
-from distutils.core import setup
-setup (name='libtaxii',
-       version='0.1',
-       description='TAXII library',
-       author='Mark Davidson',
-       author_email='mdavidson@mitre.org',
-       url='http://taxii.mitre.org/',
-       py_modules=['libtaxii.taxii_message_converter', 'libtaxii.taxii_client']
-       )
+#!/usr/bin/env python
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info <(2, 6):
+    raise Exception('libtaxii requires Python 2.6 or higher.')
+
+install_requires = ['lxml>=2.3.2', 'M2Crypto>=0.21.1']
+
+setup(name='libtaxii',
+      description='TAXII Library.',
+      author='Mark Davidson',
+      author_email='mdavidson@mitre.org',
+      url="http://taxii.mitre.org/",
+      version='0.1',
+      py_modules=['libtaxii.taxii_client', 'libtaxii.taxii_message_converter'],
+      install_requires=install_requires,
+      data_files=[('xsd', ['xsd/TAXII_XML_10.xsd',
+                           'xsd/xmldsig-core-schema.xsd'])],
+      long_description=open("README").read(),
+      keywords="taxii libtaxii",
+      )
