4c2191ab24a2c016aa5e7dc150a39d2adf7694f6
MarkDavidson <mdavidson@mitre.org>
2013-05-15 13:38:56 -0400

    Added functionality for TAXII 1.0

---
+++ libtaxii/__init__.py
@@ -0,0 +1,19 @@
+#Copyright (C) 2013 - The MITRE Corporation
+#For license information, see the LICENSE.txt file
+
+### Contributors ###
+#Contributors: If you would like, add your name to the list, alphabetically by last name
+#
+# Mark Davidson - mdavidson@mitre.org
+#
+
+#TAXII Version IDs
+VID_TAXII_SERVICES_10 = 'urn:taxii.mitre.org:services:1.0'
+VID_TAXII_XML_10 = 'urn:taxii.mitre.org:message:xml:1.0'
+VID_TAXII_HTTP_10 = 'urn:taxii.mitre.org:protocol:http:1.0'
+VID_TAXII_HTTPS_10 = 'urn:taxii.mitre.org:protocol:https:1.0'
+
+#TAXII Content Bindings
+CB_STIX_XML_10 = 'urn:stix.mitre.org:xml:1.0'
+CB_CAP_11 = 'urn:oasis:names:tc:emergency:cap:1.1'
+CB_XENC_122002 = 'http://www.w3.org/2001/04/xmlenc#'
