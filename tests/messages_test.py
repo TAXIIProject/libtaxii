040ff29e4075036e019760cd2298160bea1f68b5
MarkDavidson <mdavidson@mitre.org>
2013-05-16 08:21:20 -0400

    Aligned tests with updated messages.py

--- tests/messages_test.py
+++ tests/messages_test.py
@@ -61,123 +61,103 @@ def perform_tests(taxii_message):
     
     print '***** All tests completed!'
 
-## Discovery Request
-discovery_request_kwargs = {}
-discovery_request_kwargs['message_id'] = tm.generate_message_id() #Required
-discovery_request_kwargs['extended_headers'] = {'ext_header1': 'value1', 'ext_header2': 'value2'} #Optional
-
 #TODO: Do it like the line below this.
 #TODO: Say which args are optional
 #tm.DiscoveryRequest(message_id=tm.generate_message_id(), ... )
-discovery_request1 = tm.DiscoveryRequest(**discovery_request_kwargs)
+
+## Discovery Request
+discovery_request1 = tm.DiscoveryRequest(message_id = tm.generate_message_id(), #Required
+                                         extended_headers={'ext_header1': 'value1', 'ext_header2': 'value2'})#Optional. 
+                                         #Extended headers are optional for every message type, but only demonstrated here
 
 perform_tests(discovery_request1)
 
 ## Discovery Response
-service_kwargs = {}
-service_kwargs['service_type'] = tm.SVC_INBOX
-service_kwargs['service_version'] = t.VID_TAXII_SERVICES_10
-service_kwargs['protocol_binding'] = t.VID_TAXII_HTTP_10
-service_kwargs['address'] = 'http://example.com/inboxservice/'
-service_kwargs['message_bindings'] = [t.VID_TAXII_XML_10, 'temporaryvalue']
-service_kwargs['available'] = 'true'
-service_kwargs['content_bindings'] = []
-service_kwargs['message'] = 'This is a message.'
-service_instance1 = tm.DiscoveryResponse.ServiceInstance(**service_kwargs)
 
-discovery_response_kwargs = {}
-discovery_response_kwargs['message_id'] = tm.generate_message_id()#Required
-discovery_response_kwargs['in_response_to'] = tm.generate_message_id()#Required
-discovery_response_kwargs['service_instances'] = [service_instance1]#Optional
-discovery_response1 = tm.DiscoveryResponse(**discovery_response_kwargs)
+#Create a service instance
+service_instance1 = tm.DiscoveryResponse.ServiceInstance(service_type = tm.SVC_INBOX,#Required
+                                                         services_version = t.VID_TAXII_SERVICES_10,#Required
+                                                         protocol_binding = t.VID_TAXII_HTTP_10,#Required
+                                                         service_address = 'http://example.com/inboxservice/',#Required
+                                                         message_bindings = [t.VID_TAXII_XML_10],#Required, must have at least one value in the list
+                                                         inbox_service_accepted_content = [t.CB_STIX_XML_10],#Optional for service_type=SVC_INBOX, prohibited otherwise
+                                                                                             #If this is absent and service_type=SVC_INBOX,
+                                                                                             #It means the inbox service accepts all content
+                                                         available = True,#Optional - defaults to None, which means 'Unknown'
+                                                         message = 'This is a message.')#Optional
+
+#Create the discovery response
+discovery_response1 = tm.DiscoveryResponse(message_id = tm.generate_message_id(),#Required
+                                           in_response_to = tm.generate_message_id(),#Required. This should be the ID of the corresponding request
+                                           service_instances = [service_instance1])#Optional.
 
 perform_tests(discovery_response1)
 
 ##Feed Information Request
-feed_information_request_kwargs = {}
-feed_information_request_kwargs['message_id'] = tm.generate_message_id()
-feed_information_request1 = tm.FeedInformationRequest(**feed_information_request_kwargs)
+feed_information_request1 = tm.FeedInformationRequest(message_id = tm.generate_message_id())#Required
 
 perform_tests(feed_information_request1)
 
 ## Feed Information Response
 
-push_method_kwargs = {}
-push_method_kwargs['protocol_binding'] = t.VID_TAXII_HTTP_10
-push_method_kwargs['message_bindings'] = [t.VID_TAXII_XML_10]
-push_method1 = tm.FeedInformationResponse.Feed.PushMethod(**push_method_kwargs)
+push_method1 = tm.FeedInformationResponse.FeedInformation.PushMethod(push_protocol = t.VID_TAXII_HTTP_10, #Required
+                                                                     push_message_bindings = [t.VID_TAXII_XML_10])#Required
 
-polling_service_kwargs = {}
-polling_service_kwargs['protocol_binding'] = t.VID_TAXII_HTTP_10
-polling_service_kwargs['address'] = 'http://example.com/PollService/'
-polling_service_kwargs['message_bindings'] = [t.VID_TAXII_XML_10]
-polling_service1 = tm.FeedInformationResponse.Feed.PollingService(**polling_service_kwargs)
+polling_service1 = tm.FeedInformationResponse.FeedInformation.PollingServiceInstance(poll_protocol = t.VID_TAXII_HTTP_10,#Required
+                                                                                     poll_address = 'http://example.com/PollService/',#Required
+                                                                                     poll_message_bindings = [t.VID_TAXII_XML_10])#Required
 
-subscription_service_kwargs = {}
-subscription_service_kwargs['protocol_binding'] = t.VID_TAXII_HTTP_10
-subscription_service_kwargs['address'] = 'http://example.com/SubsService/'
-subscription_service_kwargs['message_bindings'] = [t.VID_TAXII_XML_10]
-subscription_service1 = tm.FeedInformationResponse.Feed.SubscriptionService(**subscription_service_kwargs)
+subscription_service1 = tm.FeedInformationResponse.FeedInformation.SubscriptionMethod(subscription_protocol = t.VID_TAXII_HTTP_10,#Required
+                                                                                      subscription_address = 'http://example.com/SubsService/',#Required
+                                                                                      subscription_message_bindings = [t.VID_TAXII_XML_10])#Required
 
-feed_kwargs = {}
-feed_kwargs['feed_name'] = 'Feed1'
-feed_kwargs['description'] = 'Description of a feed'
-feed_kwargs['available'] = 'true'
-feed_kwargs['content_bindings'] = [t.CB_STIX_XML_10]
-feed_kwargs['push_methods'] = [push_method1]
-feed_kwargs['polling_services'] = [polling_service1]
-feed_kwargs['subscription_services'] = [subscription_service1]
-feed1 = tm.FeedInformationResponse.Feed(**feed_kwargs)
+feed1 = tm.FeedInformationResponse.FeedInformation(feed_name = 'Feed1',#Required
+                                                   feed_description = 'Description of a feed',#Required
+                                                   supported_contents = [t.CB_STIX_XML_10],#Required. List of supported content binding IDs
+                                                   available = True,#Optional. Defaults to None (aka Unknown)
+                                                   push_methods = [push_method1],#Required if there are no polling_services. Optional otherwise
+                                                   polling_service_instances = [polling_service1],#Required if there are no push_methods. Optional otherwise.
+                                                   subscription_methods = [subscription_service1])#Optional
 
-feed_information_response_kwargs = {}
-feed_information_response_kwargs['message_id'] = tm.generate_message_id()
-feed_information_response_kwargs['in_response_to'] = tm.generate_message_id()
-feed_information_response_kwargs['extended_headers'] = {}
-feed_information_response_kwargs['feeds'] = [feed1]
-feed_information_response1 = tm.FeedInformationResponse(**feed_information_response_kwargs)
+feed_information_response1 = tm.FeedInformationResponse(message_id = tm.generate_message_id(),#Required
+                                                        in_response_to = tm.generate_message_id(),#Required. This should be the ID of the corresponding request
+                                                        feed_informations=[feed1])#Optional
 
 perform_tests(feed_information_response1)
 
 ## Poll Request
-poll_request_kwargs = {}
-poll_request_kwargs['message_id'] = tm.generate_message_id()
-poll_request_kwargs['feed_name'] = 'TheFeedToPoll'
-poll_request_kwargs['subscription_id'] = 'SomeID'
-poll_request_kwargs['exclusive_begin_timestamp'] = datetime.datetime.now()
-poll_request_kwargs['inclusive_end_timestamp'] = datetime.datetime.now()
-poll_request_kwargs['content_bindings'] = []
 
-poll_request1 = tm.PollRequest(**poll_request_kwargs)
+poll_request1 = tm.PollRequest(message_id = tm.generate_message_id(),#Required
+                               feed_name = 'TheFeedToPoll',#Required
+                               subscription_id = 'SubsId002',#Optional
+                               exclusive_begin_timestamp_label = datetime.datetime.now(),#Optional - Absence means 'no lower bound'
+                               inclusive_end_timestamp_label = datetime.datetime.now(),#Optional - Absence means 'no upper bound'
+                               content_bindings = [t.CB_STIX_XML_10])#Optional - defaults to accepting all content bindings
 
 perform_tests(poll_request1)
 
 ## Poll Response
 
-xml_content_block_kwargs = {}
-xml_content_block_kwargs['content_binding'] = t.CB_STIX_XML_10
-xml_content_block_kwargs['content'] = etree.parse(StringIO.StringIO('<stix:STIX_Package xmlns:stix="http://stix.mitre.org/stix-1"/>')).getroot()
-xml_content_block_kwargs['padding'] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-xml_content_block_kwargs['timestamp_label'] = datetime.datetime.now()
-xml_content_block1 = tm.ContentBlock(**xml_content_block_kwargs)
+stix_etree = etree.parse(StringIO.StringIO('<stix:STIX_Package xmlns:stix="http://stix.mitre.org/stix-1"/>')).getroot()
 
-string_content_block_kwargs = {}
-string_content_block_kwargs['content_binding'] = 'string'
-string_content_block_kwargs['content'] = 'Sample content'
-string_content_block_kwargs['padding'] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-string_content_block_kwargs['timestamp_label'] = datetime.datetime.now()
-string_content_block1 = tm.ContentBlock(**string_content_block_kwargs)
+xml_content_block1 = tm.ContentBlock(content_binding = t.CB_STIX_XML_10,#Required
+                                     content = stix_etree,#Required. Can be etree or string
+                                     padding = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',#Optional
+                                     timestamp_label = datetime.datetime.now())#Optional
 
-poll_response_kwargs = {}
-poll_response_kwargs['message_id'] = tm.generate_message_id()
-poll_response_kwargs['in_response_to'] = tm.generate_message_id()
-poll_response_kwargs['feed_name'] = 'FeedName'
-poll_response_kwargs['inclusive_end_timestamp'] = datetime.datetime.now()
-poll_response_kwargs['subscription_id'] = 'SubsId001'
-poll_response_kwargs['message'] = 'This is a message'
-poll_response_kwargs['inclusive_begin_timestamp'] = datetime.datetime.now()
-poll_response_kwargs['content_blocks'] = [xml_content_block1, string_content_block1]
+string_content_block1 = tm.ContentBlock(content_binding = 'string',#Required
+                                        content = 'Sample content',#Required
+                                        padding = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',#Optional
+                                        timestamp_label = datetime.datetime.now())#Optional
 
-poll_response1 = tm.PollResponse(**poll_response_kwargs)
+poll_response1 = tm.PollResponse(message_id = tm.generate_message_id(),#Required
+                                 in_response_to = tm.generate_message_id(),#Required - this should be the ID of the corresponding request
+                                 feed_name = 'FeedName',#Required
+                                 inclusive_end_timestamp_label = datetime.datetime.now(),#Required
+                                 inclusive_begin_timestamp_label = datetime.datetime.now(),#Optional
+                                 subscription_id = 'SubsId001',#Optional
+                                 message = 'This is a message.',#Optional
+                                 content_blocks = [xml_content_block1, string_content_block1])#Optional
 
 perform_tests(poll_response1)
 
@@ -189,65 +169,57 @@ status_message_kwargs['status_type'] = tm.ST_SUCCESS
 status_message_kwargs['status_detail'] = 'Real info should go here!'
 status_message_kwargs['message'] = 'This is a message.'
 
-status_message1 = tm.StatusMessage(**status_message_kwargs)
+status_message1 = tm.StatusMessage(message_id = tm.generate_message_id(),#Required
+                                   in_response_to = tm.generate_message_id(),#Required. Should be the ID of the corresponding request
+                                   status_type = tm.ST_SUCCESS,#Required
+                                   status_detail = 'Machine-processable info here!',#May be optional or not allowed, depending on status_type
+                                   message = 'This is a message.')#Optional
 
 perform_tests(status_message1)
 
 ## Inbox Message
-subscription_information_kwargs = {}
-subscription_information_kwargs['feed_name'] = 'SomeFeedName'
-subscription_information_kwargs['subscription_id'] = 'SubsId001'
-subscription_information_kwargs['inclusive_begin_timestamp_label'] = datetime.datetime.now()
-subscription_information_kwargs['inclusive_end_timestamp_label'] = datetime.datetime.now()
 
-subscription_information1 = tm.InboxMessage.SubscriptionInformation(**subscription_information_kwargs)
+subscription_information1 = tm.InboxMessage.SubscriptionInformation(feed_name = 'SomeFeedName',#Required
+                                                                    subscription_id = 'SubsId021',#Required
+                                                                    inclusive_begin_timestamp_label = datetime.datetime.now(),#Optional - Absence means 'no lower bound'
+                                                                    inclusive_end_timestamp_label = datetime.datetime.now())#Optional - Absence means 'no upper bound'
 
-inbox_message_kwargs = {}
-inbox_message_kwargs['message_id'] = tm.generate_message_id()
-inbox_message_kwargs['content_blocks'] = [xml_content_block1, string_content_block1]
-inbox_message_kwargs['message'] = 'This is a message'
-inbox_message_kwargs['subscription_information'] = subscription_information1
-
-inbox_message1 = tm.InboxMessage(**inbox_message_kwargs)
+inbox_message1 = tm.InboxMessage(message_id = tm.generate_message_id(),#Required
+                                 message = 'This is a message.',#Optional
+                                 subscription_information = subscription_information1,#Optional
+                                 content_blocks = [xml_content_block1, string_content_block1])#Optional
 
 perform_tests(inbox_message1)
 
 ## Manage Feed Subscription Request 
 
-delivery_parameters_kwargs = {}
-delivery_parameters_kwargs['inbox_protocol'] = t.VID_TAXII_HTTP_10
-delivery_parameters_kwargs['inbox_address'] = 'http://example.com/inbox'
-delivery_parameters_kwargs['delivery_message_binding'] = t.VID_TAXII_XML_10
-delivery_parameters_kwargs['content_bindings'] = [t.CB_STIX_XML_10]
+delivery_parameters1 = tm.DeliveryParameters(inbox_protocol = t.VID_TAXII_HTTP_10,#Required if requesting push messaging
+                                             inbox_address = 'http://example.com/inbox',#Yes, if requesting push messaging
+                                             delivery_message_binding = t.VID_TAXII_XML_10,#Required if requesting push messaging
+                                             content_bindings = [t.CB_STIX_XML_10])#Optional - absence means accept all content bindings
 
-delivery_parameters1 = tm.DeliveryParameters(**delivery_parameters_kwargs)
-
-manage_feed_subscription_request_kwargs = {}
-manage_feed_subscription_request_kwargs['message_id'] = tm.generate_message_id()
-manage_feed_subscription_request_kwargs['feed_name'] = 'SomeFeedName'
-manage_feed_subscription_request_kwargs['action'] = tm.ACT_SUBSCRIBE
-manage_feed_subscription_request_kwargs['subscription_id'] = 'SubsId002'
-manage_feed_subscription_request_kwargs['delivery_parameters'] = delivery_parameters1
-
-manage_feed_subscription_request1 = tm.ManageFeedSubscriptionRequest(**manage_feed_subscription_request_kwargs)
+manage_feed_subscription_request1 = tm.ManageFeedSubscriptionRequest(message_id = tm.generate_message_id(),#Required
+                                                                     feed_name = 'SomeFeedName', #Required
+                                                                     action = tm.ACT_UNSUBSCRIBE,#Required
+                                                                     subscription_id = 'SubsId056',#Required for unsubscribe, prohibited otherwise
+                                                                     delivery_parameters = delivery_parameters1)#Required
 
 perform_tests(manage_feed_subscription_request1)
 
 ## Manage Feed Subscription Response
 
-poll_instance_kwargs = {}
-poll_instance_kwargs['protocol_binding'] = t.VID_TAXII_HTTP_10
-poll_instance_kwargs['poll_address'] = 'http://example.com/poll'
-poll_instance_kwargs['poll_message_bindings'] = [t.VID_TAXII_XML_10]
-
-poll_instance1 = tm.ManageFeedSubscriptionResponse.PollInstance(**poll_instance_kwargs)
+poll_instance1 = tm.ManageFeedSubscriptionResponse.PollInstance(poll_protocol = t.VID_TAXII_HTTP_10,#Required
+                                                                poll_address = 'http://example.com/poll',#Required
+                                                                poll_message_bindings = [t.VID_TAXII_XML_10])#Required
 
 subscription_instance_kwargs = {}
 subscription_instance_kwargs['subscription_id'] = 'SubsId005'
 subscription_instance_kwargs['delivery_parameters'] = [delivery_parameters1]
 subscription_instance_kwargs['poll_instances'] = [poll_instance1]
 
-subscription_instance1 = tm.ManageFeedSubscriptionResponse.SubscriptionInstance(**subscription_instance_kwargs)
+subscription_instance1 = tm.ManageFeedSubscriptionResponse.SubscriptionInstance(subscription_id = 'SubsId234',#required
+                                                                                delivery_parameters = [delivery_parameters1],#Required if message is responding to a status action. Optional otherwise
+                                                                                poll_instances = [poll_instance1])#Required if action was polling subscription. Optional otherwise
 
 manage_feed_subscription_response_kwargs = {}
 manage_feed_subscription_response_kwargs['message_id'] = tm.generate_message_id()
@@ -256,7 +228,11 @@ manage_feed_subscription_response_kwargs['feed_name'] = 'Feed001'
 manage_feed_subscription_response_kwargs['message'] = 'This is a message.'
 manage_feed_subscription_response_kwargs['subscription_instances'] = [subscription_instance1]
 
-manage_feed_subscription_response1 = tm.ManageFeedSubscriptionResponse(**manage_feed_subscription_response_kwargs)
+manage_feed_subscription_response1 = tm.ManageFeedSubscriptionResponse(message_id = tm.generate_message_id(),#Required
+                                                                       in_response_to = tm.generate_message_id(),#Required - Should be the ID of the corresponding request
+                                                                       feed_name = 'Feed001',#Required
+                                                                       message = 'This is a message',#Optional
+                                                                       subscription_instances = [subscription_instance1])#Required
 
 perform_tests(manage_feed_subscription_response1)
 
