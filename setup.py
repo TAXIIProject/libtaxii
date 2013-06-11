e8e09a1c1c8a3d1f6c0022bd39beebfb857bc471
Greg Back <gback@mitre.org>
2013-06-10 23:29:26 -0400

    Clean up setup.py and make sure xsd files are included in sdist

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 #Copyright (C) 2013 - The MITRE Corporation
 #For license information, see the LICENSE.txt file
+import os
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
-if sys.version_info <(2, 6):
+if sys.version_info < (2, 6):
     raise Exception('libtaxii requires Python 2.6 or higher.')
 
 install_requires = ['lxml>=2.3.2', 'M2Crypto>=0.21.1', 'python-dateutil>=1.5']
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    long_description = f.read()
 
 setup(name='libtaxii',
       description='TAXII Library.',
@@ -19,10 +20,9 @@ setup(name='libtaxii',
       author_email='mdavidson@mitre.org',
       url="http://taxii.mitre.org/",
       version='1.0.100',
-      py_modules=['libtaxii.clients', 'libtaxii.messages'],
+      packages=find_packages(),
       install_requires=install_requires,
-      data_files=[('xsd', ['xsd/TAXII_XMLMessageBinding_Schema.xsd',
-                           'xsd/xmldsig-core-schema.xsd'])],
-      long_description=open("README").read(),
+      package_data={'libtaxii': ['xsd/*.xsd']},
+      long_description=long_description,
       keywords="taxii libtaxii",
       )
