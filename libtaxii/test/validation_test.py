8cf995fa1f66c1809adc5580e1bd05cafcbc8be0
Greg Back <gback@mitre.org>
2015-02-09 12:31:13 -0600

    Add failing test to reproduce #166.

---
+++ libtaxii/test/validation_test.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2105 - The MITRE Corporation
+# For license information, see the LICENSE.txt file
+
+import unittest
+
+from libtaxii.validation import do_check, message_id_regex_10
+
+
+class ValidationTests(unittest.TestCase):
+
+    def test_numeric_regex_valid(self):
+        # This message ID is valid. It should not raise an exception
+        do_check("12345", "Test Value", regex_tuple=message_id_regex_10)
+
+    # See: https://github.com/TAXIIProject/libtaxii/issues/166
+    def test_numeric_regex_invalid_end(self):
+        # This message ID is not valid.
+        args = ("12345abcd", "Message ID")
+        kwargs = {'regex_tuple': message_id_regex_10}
+        self.assertRaises(ValueError, do_check, *args, **kwargs)
+
+
+if __name__ == '__main__':
+    unittest.main()
