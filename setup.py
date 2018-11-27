6fe8e1ee56c6ec8809e3c442e8a6fd6e24096173
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2018-11-27 08:53:37 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,11 @@ def get_version():
         raise AttributeError("Package does not have a __version__")
 
 
+def get_long_description():
+    with open('README.rst') as f:
+        return f.read()
+
+
 py_maj, py_minor = sys.version_info[:2]
 if (py_maj, py_minor) < (2, 6) or (py_maj == 3 and py_minor < 3):
     raise Exception('libtaxii requires Python 2.6, 2.7 or 3.3+')
@@ -36,17 +41,15 @@ try:
 except ImportError:
     install_requires.append('argparse')
 
-with open("README.rst") as f:
-    long_description = f.read()
-
 setup(
     name='libtaxii',
     description='TAXII Library.',
     author='The MITRE Corporation',
     author_email='taxii@mitre.org',
-    url="https://taxiiproject.github.io/",
+    url='https://taxiiproject.github.io/',
     version=get_version(),
     packages=find_packages(),
+    license='BSD',
     install_requires=install_requires,
     scripts=[
         'libtaxii/scripts/collection_information_client.py',
@@ -75,8 +78,8 @@ setup(
         ],
     },
     package_data={'libtaxii': ['xsd/*.xsd']},
-    long_description=long_description,
-    keywords="taxii libtaxii",
+    long_description=get_long_description(),
+    keywords='taxii libtaxii',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -90,5 +93,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
