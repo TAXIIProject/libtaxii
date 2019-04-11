196bc29165a6f96ec77c642db02dbfdaf046bc58
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2019-04-11 14:34:42 -0400

    fix path to test configuration file

--- libtaxii/test/argument_parser_test.py
+++ libtaxii/test/argument_parser_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import libtaxii.scripts.poll_client as pc11
@@ -5,10 +6,17 @@ import libtaxii.scripts.poll_client as pc11
 
 class ArgumentParserTests(unittest.TestCase):
 
+    def setUp(self):
+        self.module_path = os.path.dirname(__file__)
+
     def test_valid_argument_passing_poll_client11(self):
         script = pc11.PollClient11Script()
         arg_parse = script.get_arg_parser(script.parser_description, path=script.path)
-        namespace = arg_parse.parse_args(["--from-file", "data/configuration.ini"])
+        namespace = arg_parse.parse_args(
+            [
+                "--from-file", os.path.join(self.module_path, "data", "configuration.ini")
+            ]
+        )
         self.assertEqual(namespace.url, "http://hailataxii2.com:80")
         self.assertEqual(namespace.path, "/taxii-data")
         self.assertEqual(namespace.port, 80)
@@ -17,7 +25,12 @@ class ArgumentParserTests(unittest.TestCase):
     def test_argument_override_poll_client11(self):
         script = pc11.PollClient11Script()
         arg_parse = script.get_arg_parser(script.parser_description, path=script.path)
-        namespace = arg_parse.parse_args(["-u", "http://hailataxii.com:80", "--from-file", "data/configuration.ini"])
+        namespace = arg_parse.parse_args(
+            [
+                "-u", "http://hailataxii.com:80",
+                "--from-file", os.path.join(self.module_path, "data", "configuration.ini")
+            ]
+        )
         self.assertEqual(namespace.url, "http://hailataxii2.com:80")  # note the argument was overwritten
         self.assertEqual(namespace.path, "/taxii-data")
         self.assertEqual(namespace.port, 80)
